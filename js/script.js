2ae12af31254c2ecb9da8a81ed6b56d35e54420e
Kayla Baca <119377119+kayharr@users.noreply.github.com>
2024-07-14 13:28:23 -0500

    added celebrate class for when hours studied are greater then weekly goal

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+const body = document.querySelector("body");
+const success = document.querySelector(".success");
+const dance = document.querySelector(".dance");
 
-const learning = {};
-
-learning.topic = "JS";
-learning.learningGoals = [
-    "Build JS Programs",
-    "Use AI to be a more well rounded developer",
-    "Create full functional websites",
-    "Learn how to integrate data into sites",
-];
-learning.category = "Front End Development";
-learning.topicImportance = "High";
-
-
-// console.log(learning);
-
-learning.topic = "JavaScript";
-learning.learningGoals.splice(1,1);
+const learning = {
+  topic: "JS",
+  learningGoals: [
+    "Understand programming fundamentals",
+    "Have Fun",
+    "Build JS programs"
+  ],
+  category: "Front End Development",
+  topicImportance: "high",
+  hoursThisWeek: 2,
+  weeklyHourGoal: 7,
+  acheivedStudyGoal: false,
+  addStudyTime: function (hours) {
+    this.hoursThisWeek += hours;
+    if (this.hoursThisWeek >= this.weeklyHourGoal) {
+        this.acheivedStudyGoal = true;
+    this.celebrate();
+  }
+},
+  celebrate: function () {
+    body.classList.add("celebrate");
+    success.classList.remove("hide");
+    dance.classList.remove("hide");
+  }
+};
 
 console.log(learning);
 
-topicElement.innerText = `I'm learning ${learning.topic}.`;
-topicElement.classList.remove("hide");
+topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
+countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
 
-countElement.innerText = `I have ${learning.learningGoals.length} learning goals.`;
+topicElement.classList.remove("hide");
 countElement.classList.remove("hide");
+
+learning.addStudyTime(9);
